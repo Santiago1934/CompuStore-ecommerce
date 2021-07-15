29590d8afcdb792185f7b01b5874b4bcea9ee728
Santiago <santiagorosales1@outlook.com>
2021-07-15 17:18:57 -0300

    deploy

--- client/src/components/Alerts/PopUp.tsx
+++ client/src/components/Alerts/PopUp.tsx
@@ -15,39 +15,6 @@ interface DetailsProduct {
 
 const PopUp = () => {
 
-    /* let id = 19
-
-    const { data } = useQuery<DetailsProduct>(GET, {
-        variables: { id }
-    });
-
-    let productPopUp = data?.getProductById
-
-    return (
-        <div className={styles.OrderPop} >
-            <div className={styles.box} >
-                <div className={styles.content} >
-                    <h1 className={styles.PopTitle} >Producto en oferta </h1>
-                    <h1 className={styles.PPop} >{productPopUp?.name}</h1>
-                </div>
-            </div>
-            <div className={styles.box2} >
-                <div className={styles.content2}>
-                    <img className={styles.South1} src={South} alt='' />
-                    <Link className={styles.ImagePopL} to={{
-                        pathname: '/Detalles',
-                        state: {
-                            id: id,
-                        }
-                    }}>
-                        <img className={styles.ImagePop} src={productPopUp?.image} alt='' />
-                    </Link>
-                    <img className={styles.South2} src={South} alt='' />
-
-                </div>
-            </div>
-        </div>
-    ) */
 }
 
 export default PopUp
